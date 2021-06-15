06cb000867c9d6967fa79972f3beb40ad9520e82
Pritam <pritampatel4872@gmail.com>
2021-06-15 13:33:02 +0530

    first commit

---
+++ src/main/java/com/sapient/programs/App.java
@@ -0,0 +1,27 @@
+package com.sapient.programs;
+
+import java.util.logging.Logger;
+
+/**
+ * Hello world!
+ */
+public final class App
+ {
+     static Logger log =Logger.getAnonymousLogger();
+     public App() {
+    }
+public long factorial(int num)
+{
+    long f=1;
+    while(num>1)
+    {
+        f*=num--;
+    }
+    return f;
+}
+    
+    public static void main(String[] args) {
+        //System.out.println("Hello World!")
+        log.info("HeLLO WORLD");
+    }
+}
---
+++ src/test/java/com/sapient/AppTest.java
@@ -0,0 +1,25 @@
+package com.sapient;
+
+import static org.junit.Assert.assertEquals;
+
+import com.sapient.programs.App;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+/**
+     * Rigorous Test.
+     */
+@Test
+   public void testFactorialForNumberBelowTen() 
+   {
+       App app =new App();
+       long expected=120;
+       long actual= app.factorial(5);
+       assertEquals(expected, actual);// assertEquals(1, 1);
+    }
+}
